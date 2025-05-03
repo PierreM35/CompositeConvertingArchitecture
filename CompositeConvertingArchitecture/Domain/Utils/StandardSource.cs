0b7cf7ef2f1fdb73b462aad1c09acacdda364d2b
menardp21 <menardp21@gmail.com>
2025-05-03 14:45:23 +0200

    Add Std1 to Standards

--- CompositeConvertingArchitecture/Domain/Utils/StandardSource.cs
+++ CompositeConvertingArchitecture/Domain/Utils/StandardSource.cs
@@ -1,10 +1,11 @@
 ﻿using CompositeConvertingArchitecture.Domain.Abstractions;
+using CompositeConvertingArchitecture.Domain.Model.Standards;
 
 namespace CompositeConvertingArchitecture.Domain.Utils
 {
     public static class StandardSource
     {
-        public static IList<Standard> Standards { get; } = new List<Standard>();
+        public static IList<Standard> Standards { get; } = [new Standard1()];
 
     }
 }
