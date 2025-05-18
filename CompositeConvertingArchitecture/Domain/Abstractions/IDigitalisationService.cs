13c51e672c3963727eb58af811885f3d0e00e876
menardp21 <menardp21@gmail.com>
2025-05-18 23:52:06 +0200

    rename

---
+++ CompositeConvertingArchitecture/Domain/Abstractions/IDigitalisationService.cs
@@ -0,0 +1,11 @@
+﻿using ModelDigitalisationArchitecture.Domain.Model;
+using ModelDigitalisationArchitecture.Infrastructure.Coding.Model;
+
+namespace ModelDigitalisationArchitecture.Domain.Abstractions
+{
+    internal interface IDigitalisationService
+    {
+        Message Decode(Code code);
+        Code Encode(Message message);
+    }
+}
